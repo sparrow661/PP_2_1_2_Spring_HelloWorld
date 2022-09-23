3c4a69e302fda22f5d3a7ece1d8852b3f0a03662
sparrow661 <vorobei.pavel2@gmail.com>
2022-09-23 18:53:01 +0300

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat bean3 =
+                (Cat) applicationContext.getBean("Cat");
+        System.out.println(bean3.getVoice());
+        Cat bean4 =
+                (Cat) applicationContext.getBean("Cat");
+        System.out.println(bean4.getVoice());
+        System.out.println(bean1 == bean2);
+        System.out.println(bean3 == bean4);
+
     }
 }
